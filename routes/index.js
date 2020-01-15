4b42a68416f1a03515f18315224816f8ee50ec81
kilianal <kilianal@mediamarktsaturn.com>
2020-01-15 16:57:36 +0100

    definition routes and related hbs

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+
+//Seite zu Über uns entspricht dem Impressum
+router.get("/aboutUs", (req,res,next)=>{
+res.render("aboutUs")
+})
+
+//Hier wird eine Liste von Links für alle öffentlich angezeigt
+router.get("/links", (req,res,next)=>{
+  res.render("links")
+  })
+
+
+  router.get("/gallery", (req,res,next)=>{
+    res.render("gallery")
+    })
+
 module.exports = router;
