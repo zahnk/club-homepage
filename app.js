e067c207341c16da35469482007e56fa46c5bdf5
MSHSusanneV <voglsus@mediamarktsaturn.com>
2020-01-16 16:49:31 +0100

    Icon added

--- app.js
+++ app.js
@@ -58,8 +58,8 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 
-// default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+// value for title local
+app.locals.title = 'IRONGarden';
 
 
 
