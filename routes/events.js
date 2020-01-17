2dfe2c6af9454afb507e93f42fd402946c5bdc39
kilianal <kilianal@mediamarktsaturn.com>
2020-01-17 10:41:46 +0100

    event und service routen getrennt

--- routes/events.js
+++ routes/events.js
@@ -22,17 +22,9 @@ router.get("/private/userStartSeite", (req, res, next) => {
     res.render("private/userStartSeite", {user: user});
 });
 
-//Nur wer eingelogged ist und Adminrechte hat, hat Zugriff auf die Adminseite
-router.get("/private/admin", (req, res, next) => {
-if(req.session.currentUser.userrole=="admin"){
-    res.render("private/admin");
-}
-else{
-    res.redirect("/");
-}
+router.get("/private/events", (req, res, next) => {
+  res.render("private/events");
 });
-
-
 
 
 module.exports = router;
